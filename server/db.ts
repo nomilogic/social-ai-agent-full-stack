15629018151ec1717755b5fc5a7f02508453c8b6
saltest47 <saltest47@gmail.com>
2025-08-15 23:43:59 +0500

    Updated db.ts

--- server/db.ts
+++ server/db.ts
@@ -2,14 +2,14 @@ import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "../shared/schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error(
+//     "DATABASE_URL must be set. Did you forget to provision a database?",
+//   );
+// }
 
 export const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://postgres.kdaxthclqiodvetumxpn:Cimple%40.123@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres",
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
